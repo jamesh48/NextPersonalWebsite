11bd1d3e8265f81e0b97dd81f9e946e3b10df137
jamesh48 <jameshrivnak4@gmail.com>
2023-11-19 15:07:25 -0700

    updates to cdk fargate spot, certificate, https settings

--- iac/bin/iac.ts
+++ iac/bin/iac.ts
@@ -4,4 +4,9 @@ import * as cdk from 'aws-cdk-lib';
 import { PersonalWebsiteStack } from '../lib/personal-website-stack';
 
 const app = new cdk.App();
-new PersonalWebsiteStack(app, 'PersonalWebsiteStack', {});
+new PersonalWebsiteStack(app, 'PersonalWebsiteStack', {
+  env: {
+    account: '471507967541',
+    region: 'us-east-1',
+  },
+});
--- iac/lib/personal-website-stack.ts
+++ iac/lib/personal-website-stack.ts
@@ -1,6 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class PersonalWebsiteStack extends cdk.Stack {
@@ -11,15 +14,39 @@ export class PersonalWebsiteStack extends cdk.Stack {
       this,
       'fsh-fullstack-hrivnak-alb',
       {
-        // certificate,
+        vpc: ec2.Vpc.fromLookup(this, 'jh-imported-vpc', {
+          vpcId: 'vpc-081edc60bd2157d7e',
+        }),
+        certificate: acm.Certificate.fromCertificateArn(
+          this,
+          'fsh-imported-certificate',
+          'arn:aws:acm:us-east-1:471507967541:certificate/a1bf61c4-5380-40b8-99a5-4c0bf7d3212e'
+        ),
         loadBalancerName: 'fsh-fullstack-hrivnak-alb',
-        // redirectHTTP: true,
+        redirectHTTP: true,
         taskImageOptions: {
           image: ecs.ContainerImage.fromAsset('code/src'),
+          taskRole: iam.Role.fromRoleName(
+            this,
+            'jh-ecs-task-definition-role',
+            'jh-ecs-task-definition-role'
+          ),
+          executionRole: iam.Role.fromRoleName(
+            this,
+            'jh-ecs-task-execution-role',
+            'jh-ecs-task-execution-role'
+          ),
           environment: {
-            NEXT_PUBLIC_CLOUDFRONTLINK: 'https://static.fullstackhrivnak.com',
+            NEXT_PUBLIC_CLOUDFRONTLINK: 'https://d1y3bjxf7c78hf.cloudfront.net',
           },
         },
+        capacityProviderStrategies: [
+          {
+            capacityProvider: 'FARGATE_SPOT',
+            weight: 1,
+          },
+        ],
+        desiredCount: 1,
         enableExecuteCommand: true,
       }
     );
