3cde2e26dd63d8e76d54610314969e63b0fefc71
James Hrivnak <James.Hrivnak@united.com>
2023-12-14 20:01:18 -0700

    next routes

--- iac/lib/personal-website-stack.ts
+++ iac/lib/personal-website-stack.ts
@@ -121,7 +121,9 @@ export class PersonalWebsiteStack extends cdk.Stack {
     importedALBListener.addTargetGroups('fsh-listener-tg', {
       targetGroups: [targetGroup],
       priority: 10,
-      conditions: [elbv2.ListenerCondition.pathPatterns(['/', '/fullstack/*'])],
+      conditions: [
+        elbv2.ListenerCondition.pathPatterns(['/', '/fullstack/*', '/_next/*']),
+      ],
     });
 
     // const _fshService = new ecsPatterns.ApplicationLoadBalancedFargateService(
