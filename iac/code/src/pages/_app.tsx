cb4e6201c67794dc7b4eeb77a3ff105aaaa73e63
James Hrivnak <jameshrivnak4@gmail.com>
2026-01-24 16:04:36 -0700

    update meta tags

--- iac/code/src/pages/_app.tsx
+++ iac/code/src/pages/_app.tsx
@@ -3,9 +3,10 @@ import GlobalStore from '@app/store'
 import { useIsSsr } from '@shared/customHooks'
 import { smallWindowCheck } from '@shared/globalUtils'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { Provider } from 'react-redux'
 import Layout from '../layouts/Layout'
-import '../styles/globals.scss' // your global styles
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
 	const isSsr = useIsSsr()
@@ -25,11 +26,35 @@ function MyApp({ Component, pageProps }: AppProps) {
 	})
 
 	return (
-		<Provider store={GlobalStore.prototype.getStore()}>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</Provider>
+		<>
+			<Head>
+				<title>FullStack Hrivnak | Portfolio & Projects</title>
+				<meta
+					name="description"
+					content="Full-stack web developer specializing in modern web applications. View my portfolio, projects, and technical expertise."
+				/>
+				<meta name="author" content="James Hrivnak" />
+
+				<meta property="og:title" content="FullStack Hrivnak" />
+				<meta
+					property="og:description"
+					content="Full-stack web developer specializing in modern web applications. View my portfolio, projects, and technical expertise."
+				/>
+				<meta property="og:type" content="website" />
+
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:title" content="FullStack Hrivnak" />
+				<meta
+					name="twitter:description"
+					content="Full-stack web developer specializing in modern web applications."
+				/>
+			</Head>
+			<Provider store={GlobalStore.prototype.getStore()}>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</Provider>
+		</>
 	)
 }
 
